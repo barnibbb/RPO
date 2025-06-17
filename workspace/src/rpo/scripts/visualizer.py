c2f8dcaa7b649fd1eb1fb3f315c4265105fce5c3
barnibbb <barnibbb@gmail.com>
2025-06-17 11:01:39 +0200

    Irradiance map visualization tool

--- workspace/src/rpo/scripts/visualizer.py
+++ workspace/src/rpo/scripts/visualizer.py
@@ -2,43 +2,125 @@ import open3d as o3d
 import extended_octree_module
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
 
 # export PYTHONPATH=/home/appuser/workspace/devel/lib:$PYTHONPATH
 
-if __name__ == '__main__':
+voxel_size = 0.05
 
-    file = '/home/appuser/data/models/infirmary_extended.ot'
 
-    voxels = extended_octree_module.get_voxels(file)
 
-    print(len(voxels))
+def get_original_colors(voxels):
+    return np.array([[v["r"], v["g"], v["b"]] for v in voxels])
 
-    voxel_centers = []
+
+
+def get_height_colors(voxel_centers):
+    voxel_colors = []
+
+    z_values = voxel_centers[:, 2]
+    z_min, z_max = z_values.min(), z_values.max()
+    z_normalized = (z_values - z_min) / (z_max - z_min)
+
+    colors = np.stack([1.0 - z_normalized, 0.2 * np.ones_like(z_normalized), z_normalized], axis=1)
+
+    return np.array(voxel_colors)
+
+
+
+def get_random_colors(voxel_centers):
+    voxel_colors = np.random.rand(len(voxel_centers), 3)
+
+    return np.array(voxel_colors)
+
+
+
+def get_type_color(voxels):
     voxel_colors = []
 
     for v in voxels:
-        voxel_centers.append([v["x"], v["y"], v["z"]])
-        voxel_colors.append([v["r"], v["g"], v["b"]])
+        if v["type"] == 1:
+            voxel_colors.append([255.0 / 255.0, 120.0 / 255.0, 0.0 / 255.0])
+        elif v["type"] == 2:
+            voxel_colors.append([254.0 / 255.0, 47.0 / 255.0, 125.0 / 255.0])
+        elif v["type"] == 4:
+            voxel_colors.append([255.0 / 255.0, 120.0 / 255.0, 0.0 / 255.0])
+        elif v["type"] == 5:
+            voxel_colors.append([15.0 / 255.0, 82.0 / 255.0, 186.0 / 255.0])
+        else:
+            voxel_colors.append([0.0, 0.0, 0.0])
 
-    voxel_centers = np.array(voxel_centers)
-    voxel_colors = np.array(voxel_colors)
+    return np.array(voxel_colors)
+
+
+
+def get_irradiance_color(voxels):
+    voxel_values = np.array([v["value"] for v in voxels])
+
+    clip_value = np.percentile(voxel_values, 99)
+    voxel_values_clipped = np.clip(voxel_values, 0, clip_value)
+
+    value_min = voxel_values_clipped.min()
+    value_max = voxel_values_clipped.max()
+    value_normalized = (voxel_values_clipped - value_min) / (value_max - value_min)
+
+    colormap = plt.get_cmap("viridis")
+
+    voxel_colors = colormap(value_normalized)[:, :3]
+
+    # voxel_colors = np.stack([value_normalized, 0.0 * value_normalized, value_normalized], axis=1)
+
+    return voxel_colors
+
+
+
+if __name__ == '__main__':
+    
+    model_file = '/home/appuser/data/models/infirmary_extended.ot'
+    # irradiance_file = '/home/appuser/data/irradiance_infirmary/32751_32760.bin'
+
+    irradiance_dir = '/home/appuser/data/irradiance_infirmary'
+    irradiance_files = sorted(glob.glob(os.path.join(irradiance_dir, '*.bin')))
+    index = 0
+
+    initial_voxels = extended_octree_module.get_irradiance_values(model_file, irradiance_files[index])
+    voxel_centers = np.array([[v["x"], v["y"], v["z"]] for v in initial_voxels])
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(voxel_centers)
 
-    # z_values = voxel_centers[:, 2]
-    # z_min, z_max = z_values.min(), z_values.max()
-    # z_normalized = (z_values - z_min) / (z_max - z_min)
-
-    # colors = np.stack([1.0 - z_normalized, 0.2 * np.ones_like(z_normalized), z_normalized], axis=1)
-    #colors = np.random.rand(len(voxel_centers), 3)
+    voxel_colors = get_irradiance_color(initial_voxels)    
     pcd.colors = o3d.utility.Vector3dVector(voxel_colors)
-
-    voxel_size = 0.05
-
 
     vg = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=voxel_size)
 
-    o3d.visualization.draw_geometries([vg])
+    # o3d.visualization.draw_geometries([vg])
+
+    vis = o3d.visualization.VisualizerWithKeyCallback()
+    vis.create_window()
+    vis.add_geometry(vg)
+
+    def update_voxels(vis):
+        global index
+        index = (index + 1) % len(irradiance_files)
+        print(f"Loading: {irradiance_files[index]}")
+        voxels = extended_octree_module.get_irradiance_values(model_file, irradiance_files[index])
+        colors = get_irradiance_color(voxels)  
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+
+        new_vg = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=voxel_size)
+        vis.clear_geometries()
+        vis.add_geometry(new_vg)
+        vis.poll_events()
+        vis.update_renderer()
+
+    RIGHT_ARROW = 262
+    vis.register_key_callback(RIGHT_ARROW, update_voxels)
+
+    print("Press → to step through irradiance frames.")
+    vis.run()
+    vis.destroy_window()
 
 
--- workspace/src/rpo/src/octree_bindings.cpp
+++ workspace/src/rpo/src/octree_bindings.cpp
@@ -4,6 +4,7 @@
 #include <octomap/ColorOcTree.h>
 
 #include "extended_octree.h"
+#include "dose_calculator.h"
 
 namespace py = pybind11;
 
@@ -32,6 +33,8 @@ py::list get_voxels_from_tree(const std::string& filename)
             voxel["g"] = color.y();
             voxel["b"] = color.z();
 
+            voxel["type"] = it->getType();
+
             result.append(voxel);
         }
     }
@@ -39,9 +42,76 @@ py::list get_voxels_from_tree(const std::string& filename)
     return result;
 }
 
+
+py::list get_irradiance_values(const std::string& model_file, const std::string& irradiance_file)
+{
+    std::shared_ptr<rpo::ExtendedOcTree> extended_model = nullptr;
+
+    extended_model.reset(dynamic_cast<rpo::ExtendedOcTree*>(AbstractOcTree::read(model_file)));
+
+    py::list result;
+
+    std::ifstream file(irradiance_file, std::ios::binary);
+
+    rpo::ExposureMap irradiance_map;
+
+    if (file.is_open())
+    {
+        octomap::OcTreeKey key;
+        float value;
+
+        while (file.read(reinterpret_cast<char*>(&key[0]), sizeof(key[0])))
+        {
+            py::dict voxel;
+
+            file.read(reinterpret_cast<char*>(&key[1]), sizeof(key[1]));
+            file.read(reinterpret_cast<char*>(&key[2]), sizeof(key[2]));
+            file.read(reinterpret_cast<char*>(&value), sizeof(value));
+
+            irradiance_map[key] = value;
+        }
+
+        file.close();
+    }
+    else
+    {
+        std::cerr << "Could not open input file for loading irradiance map!" << std::endl;
+    }
+
+
+    for (rpo::ExtendedOcTree::leaf_iterator it = extended_model->begin_leafs(), end = extended_model->end_leafs(); it != end; ++it)
+    {
+        if (extended_model->isNodeOccupied(*it))
+        {
+            py::dict voxel;
+            voxel["x"] = it.getX();
+            voxel["y"] = it.getY();
+            voxel["z"] = it.getZ();
+
+            if (irradiance_map.find(it.getKey()) != irradiance_map.end())
+            {
+                voxel["value"] = irradiance_map[it.getKey()];
+            }
+            else
+            {
+                voxel["value"] = 0;
+            }
+
+            result.append(voxel);
+        }
+    }
+
+
+    return result;
+}
+
+
+
+
 PYBIND11_MODULE(extended_octree_module, m)
 {
     m.doc() = "Pybind11 bindings for custom OctoMap";
     m.def("get_voxels", &get_voxels_from_tree, "Load voxels from extended octree file.");
+    m.def("get_irradiance_values", &get_irradiance_values, "Load irradiance values from submap.");
 }
 
